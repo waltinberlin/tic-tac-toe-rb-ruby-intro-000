5396bcc9e695a2aad440e6c61ee3dbfd817785ec
Walter Minehuber <minehuber@gmail.com>
2017-09-27 19:43:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -98,3 +98,30 @@ def turn(board)
   move(board,index,"X")
   display_board(board)
 end
+
+def turn_count(board)
+  count = 0
+  board.each do |position|
+    if position == "X" || position == "O"
+      count+=1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  countX = 0
+  countO = 0
+  board.each do |position|
+    if position == "X"
+      countX+=1
+    elsif position == "O"
+      countO+=1
+    end
+  end
+  if countX > countO
+    "O"
+  else
+    "X"
+  end
+end
