680eb2c7dc7847874d4294369bdfcb2c59f019fc
Walter Minehuber <minehuber@gmail.com>
2017-09-27 19:38:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,3 +65,8 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(user_input)
+ position = user_input.to_i
+ position - 1
+end
